9bad9e4d23e404af5f9e50957192ab60ae4647c8
ddpalacios <palaciosdanieldario@gmail.com>
2020-05-01 13:04:34 -0500

    Updated Source Project

---
+++ app/src/main/java/com/example/cta_map/mainactivity.java
@@ -0,0 +1,161 @@
+package com.example.cta_map;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.SupportMapFragment;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+class Debugger{
+    void ShowToast(Context context, String text){
+        Toast toast = Toast.makeText(context, text, Toast.LENGTH_LONG);
+        toast.show();
+
+    }
+}
+@SuppressLint("Registered")
+public class mainactivity extends AppCompatActivity {
+
+    public final String url = "https://lapi.transitchicago.com/api/1.0/ttpositions.aspx?key=94202b724e284d4eb8db9c5c5d074dcd&rt=red";
+    private Button getData, closeConn, toMap;
+    private TextView result;
+    private Boolean openConnection = true;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        final Context context = getApplicationContext();
+        super.onCreate(savedInstanceState);
+        final Debugger debug = new Debugger();
+
+
+        setContentView(R.layout.activity_main);
+
+        result = (TextView) findViewById(R.id.result);
+        result.setMovementMethod(new ScrollingMovementMethod());
+        getData = (Button) findViewById(R.id.getData);
+        closeConn = (Button) findViewById(R.id.closeData);
+        toMap = (Button) findViewById(R.id.ToMaps);
+
+
+        toMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mainactivity.this, MapsActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+        getData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                debug.ShowToast(context, "Extracting Content...");
+                openConnection = true;
+                extract_train_content(url, debug);
+            }
+        });
+
+
+    }
+
+
+    private void extract_train_content(final String url, final Debugger debugger){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (openConnection) {
+
+                    try {
+                        Document content = Jsoup.connect(url).get();
+                        final Elements lat = content.select("lat");
+                        final Elements lon = content.select("lon");
+                        final Elements isApp = content.select("isApp");
+                        final Elements dest = content.select("nextStaNm");
+
+                        String latitude = lat.text();
+                        String longtitude = lon.text();
+                        String destination = dest.text();
+                        String isApproaching = isApp.text();
+                        final String[] lat_list = latitude.split(" ");
+                        final String[] lon_list = longtitude.split(" ");
+                        final String[] app_list = isApproaching.split(" ");
+                        final String[] destiniation_station = destination.split(" ");
+
+                        Log.d("Latititudes", Arrays.toString(lat_list));
+                        Log.d("Longtitude", Arrays.toString(lon_list));
+                        Log.d("Longtitude", Arrays.toString(app_list));
+
+
+                        runOnUiThread(new Runnable() {
+                            @SuppressLint("SetTextI18n")
+                            @Override
+                            public void run() {
+                                if (result.getText().toString().isEmpty()){
+                                    for (int i=0; i< lat_list.length; i++){
+
+
+                                        String isApp = app_list[i];
+                                        if (isApp.equals("1")){
+                                            result.append("\n\nTrain# "+i+"  "+lat_list[i]+"   "+ lon_list[i]+"\t\tApproaching: "+ destiniation_station[i]);
+
+
+                                        }
+                                        else{
+                                            result.append("\n\nTrain# "+i+"  "+lat_list[i]+"   "+ lon_list[i]+"\t\tNext Stop: "+ destiniation_station[i]);
+
+                                        }
+
+                                    }
+                                }
+                                else {
+                                    result.setText(null);
+                                    run();
+
+                                }
+
+                            }
+                        });
+
+                    } catch (IOException e) {
+                        Log.d("Error", "Error in extracting");
+                    }
+
+
+                }
+            }
+        }).start();
+
+        closeConn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openConnection = false;
+                result.setText(null);
+
+                debugger.ShowToast(getApplicationContext(), "Connection Closed!");
+                Log.d("Connection Status", "Connection Closed");
+            }
+        });
+
+    }
+
+
+
+
+}
+
