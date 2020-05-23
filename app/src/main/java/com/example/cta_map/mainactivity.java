741126899502f834a1a16701dec60157d3b52d64
ddpalacios <palaciosdanieldario@gmail.com>
2020-05-23 12:51:56 -0500

    Code cleanup in main

--- app/src/main/java/com/example/cta_map/mainactivity.java
+++ app/src/main/java/com/example/cta_map/mainactivity.java
@@ -52,8 +52,6 @@ class Debugger{
 public class mainactivity extends AppCompatActivity {
     private Button  toMap;
     private EditText station_name, station_type, direction;
-    int PERMISSION_ID = 44;
-    private FusedLocationProviderClient mFusedLocationClient;
 
 
     @RequiresApi(api = Build.VERSION_CODES.M)
@@ -61,20 +59,15 @@ public class mainactivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_main);
         super.onCreate(savedInstanceState);
+        Context context = getApplicationContext();
         station_name = (EditText) findViewById(R.id.station_name);
         station_type = (EditText) findViewById(R.id.station_type);
         direction = (EditText) findViewById(R.id.dest);
         toMap = (Button) findViewById(R.id.toMaps);
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        getLastLocation();
+        final UserLocation userLocation = new UserLocation(context);
+        userLocation.getLastLocation();
         headToMap();
     }
-
-
-
-
-
-
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     private void headToMap(){
@@ -85,8 +78,6 @@ public class mainactivity extends AppCompatActivity {
 
                 final Context context = getApplicationContext();
                 final Debugger debug = new Debugger();
-//                InputStream CSVfile = getResources().openRawResource(R.raw.train_stations);
-//                BufferedReader reader = new BufferedReader(new InputStreamReader(CSVfile, StandardCharsets.UTF_8));
                 final Chicago_Transits chicago_transits = new Chicago_Transits();
                 BufferedReader station_coordinates_reader = chicago_transits.setup_file_reader(context, R.raw.train_stations);
                 Intent intent = new Intent(mainactivity.this, MapsActivity.class);
@@ -111,112 +102,14 @@ public class mainactivity extends AppCompatActivity {
         });
     }
 
-
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    private  BufferedReader get_main_stations(){
-        InputStream CSVfile = getResources().openRawResource(R.raw.train_stations);
-        return new BufferedReader(new InputStreamReader(CSVfile, StandardCharsets.UTF_8));
-
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    @SuppressLint("MissingPermission")
-    private void getLastLocation(){
-        if (checkPermissions()) {
-            if (isLocationEnabled()) {
-                mFusedLocationClient.getLastLocation().addOnCompleteListener(
-                        new OnCompleteListener<Location>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Location> task) {
-                                Location location = task.getResult();
-                                if (location == null) {
-                                    requestNewLocationData();
-                                } else {
-                                    Log.e("LOC", location.getLatitude()+","+location.getLongitude());
-//                                    userLoc.setText(location.getLatitude()+","+location.getLongitude());
-
-
-                                }
-                            }
-                        }
-                );
-            } else {
-                Toast.makeText(this, "Turn on location", Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivity(intent);
-            }
-        } else {
-            requestPermissions();
-        }
-    }
-
-    @SuppressLint("MissingPermission")
-    private void requestNewLocationData(){
-
-        LocationRequest mLocationRequest = new LocationRequest();
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        mLocationRequest.setInterval(0);
-        mLocationRequest.setFastestInterval(0);
-        mLocationRequest.setNumUpdates(1);
-
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        mFusedLocationClient.requestLocationUpdates(
-                mLocationRequest, mLocationCallback,
-                Looper.myLooper()
-        );
-
-    }
-
-    private LocationCallback mLocationCallback = new LocationCallback() {
-        @Override
-        public void onLocationResult(LocationResult locationResult) {
-            Location mLastLocation = locationResult.getLastLocation();
-            Log.e("loc", (mLastLocation.getLatitude()+","+ mLastLocation.getLongitude()));
-
-        }
-    };
-
-    private boolean checkPermissions() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            return true;
-        }
-        return false;
-    }
-
-    private void requestPermissions() {
-        ActivityCompat.requestPermissions(
-                this,
-                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
-                PERMISSION_ID
-        );
-    }
-
-
-    private boolean isLocationEnabled() {
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
-                LocationManager.NETWORK_PROVIDER
-        );
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == PERMISSION_ID) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                getLastLocation();
-            }
-        }
-    }
-
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onResume(){
         super.onResume();
-        if (checkPermissions()) {
-            getLastLocation();
+        Context context = getApplicationContext();
+        final UserLocation userLocation = new UserLocation(context);
+        if (userLocation.checkPermissions()) {
+            userLocation.getLastLocation();
         }
 
     }
