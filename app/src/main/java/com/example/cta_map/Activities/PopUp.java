eef6a452d43ea13589008900723b19ccd030e4e9
ddpalacios <palaciosdanieldario@gmail.com>
2020-08-19 15:27:27 -0500

    Dropdown settings

--- app/src/main/java/com/example/cta_map/Activities/PopUp.java
+++ app/src/main/java/com/example/cta_map/Activities/PopUp.java
@@ -3,10 +3,17 @@ package com.example.cta_map.Activities;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.cta_map.R;
 
 public class PopUp  extends Activity {
+    Spinner spinner;
+    ArrayAdapter<CharSequence> adapter;
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -15,8 +22,23 @@ public class PopUp  extends Activity {
         getWindowManager().getDefaultDisplay().getMetrics(dm);
         int width = dm.widthPixels;
         int height = dm.heightPixels;
-
         getWindow().setLayout((int)(width*.8), (int)(height*.6));
+        spinner =(Spinner) findViewById(R.id.line_selection);
+        adapter = ArrayAdapter.createFromResource(this, R.array.line_names, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), parent.getItemIdAtPosition(position)+" ", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
 
 
 
