b01b34d4bec84e2715930fbd7d37fedbf9d12c71
DPalacios <palaciosdanieldario@gmail.com>
2021-04-10 08:49:05 -0500

    Added direction change

--- app/src/main/java/com/example/cta_map/Activities/MainActivity.java
+++ app/src/main/java/com/example/cta_map/Activities/MainActivity.java
@@ -281,6 +281,23 @@ public class MainActivity extends AppCompatActivity implements GoogleMap.OnMyLoc
         });
 
 
+        FloatingActionButton switch_dir = findViewById(R.id.switch_dir_button);
+        switch_dir.setOnClickListener(v -> {
+            String dir = message.getDir();
+            if (dir !=null) {
+                if (dir.equals("1")) {
+                    message.setDir("5");
+
+                } else {
+                    message.setDir("1");
+                }
+                message.getT1().interrupt();
+            }
+
+
+        });
+
+
     }
 
     private Boolean isRunning(){
